c9393b87f0c9f22374192dbbb9a3784ae588e4d9
Vitório Santos <124390765+vitorio-santos@users.noreply.github.com>
2026-07-30 21:22:31 -0300

    Update AgendamentoController.java

--- agendador-horario/src/main/java/controller/AgendamentoController.java
+++ agendador-horario/src/main/java/controller/AgendamentoController.java
@@ -11,7 +11,7 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
-@RequestMapping
+@RequestMapping(/"agendamentos)
 @RequiredArgsConstructor
 
 public class AgendamentoController {
